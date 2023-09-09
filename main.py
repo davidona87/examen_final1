120d00ef84da2e74f88385e0284e206a4fa271b2
davidona87 <daonasa@uide.edu.ec>
2023-09-09 15:10:57 -0500

    commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from db import MongoDriver
+
+driver = webdriver.Chrome()
+driver.get("https://www.muchomejorec.com/")
+search_box = driver.find_element(by=By.CSS_SELECTOR, value="#buscar_todo")
+search_box.send_keys("laptop")
+
+search_button = driver.find_element(by=By.CSS_SELECTOR, value="#search-submit > i")
+search_button.click()
+
+articles = driver.find_elements(By.CSS_SELECTOR, "body > section > section > div > div > div > section > div ")
+
+mongodb = MongoDriver()
+
+for lapt in articles:
+    try:
+        #image = card.find_element(By.CSS_SELECTOR, "div > div > div.left-block2.col-xs-4.col-xs-4.col-md-3 > div > a > img").image
+        title = lapt.find_element(By.CSS_SELECTOR, "body > section > section > div > div > div > section > div > div:nth-child(2) > div > div > div > a:nth-child(1) > div.marca > span").text
+        description = lapt.find_element(By.CSS_SELECTOR, "body > section > section > div > div > div > section > div > div:nth-child(2) > div > div > div > a:nth-child(1) > div.aparecer > p").text
+        price = lapt.find_element(By.CSS_SELECTOR, "body > section > section > div > div > div > section > div > div:nth-child(2) > div > div > div > a:nth-child(1) > h5.precio").text
+        print(title)
+        print(description)
+        print(f"{price}")
+
+        artic = {
+            "Título": title,
+            "Descripción": description,
+            "Precio": price
+        }
+
+
+
+        mongodb.insert_record(record=artic, username="suministros")
+
+        print("++++++++++++++++++++++++++++++++")
+    except Exception as e:
+        print(e)
+        print("++++++++++++++++++++++++++++++++")
+
+
+driver.close()
---
+++ readdb.py
@@ -0,0 +1,33 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user = os.getenv('MONGO_USER')
+password = os.getenv('MONGO_PASSWORD')
+hostname = os.getenv('MONGO_HOSTNAME')
+
+uri = f"mongodb+srv://{user}:{password}@{hostname}/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+def read():
+    try:
+        lectura = client.get_database('db_art').get_collection('suministros').find()
+        for file in lectura:
+            print(file)
+    except Exception as e:
+        print(e)
+
+
+read()
